2d565d6c2c1926897fa1ec24d206fe457d894d1c
Davidson Bpe <davidsomixer@gmail.com>
2021-12-04 16:29:32 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,19 +2,6 @@ var cacheName = 'notedmais-v1';
 var filesToCache = [
     './',
     './index.html',
-    './plugins/css/styles.css',
-    './bootstrap/css/bootstrap.min.css',
-    './bootstrap/font-awesome/css/font-awesome.min.css',
-    './plugins/dist/css/AdminLTE.min.css',
-    './plugins/dist/css/skins/all-skins.min.css',
-    './plugins/pace/pace.min.css',
-    'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic',
-    './plugins/jQuery/jquery-3.1.1.min.js',
-    './bootstrap/js/bootstrap.min.js',
-    './plugins/pace/pace.min.js',
-    './plugins/dist/js/adminlte.min.js',
-    './plugins/mtpdf/jspdf.debug.js',
-    './plugins/mtpdf/main.js',
     './manifest.json'
 ];
 self.addEventListener('install', function (e) {
